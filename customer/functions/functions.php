23901b2dfffe36534597d3dce2354be2f683c721
Rohit Patil <74750524+rohit1097@users.noreply.github.com>
2023-01-18 15:34:31 +0530

    Update functions.php

--- customer/functions/functions.php
+++ customer/functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "", "tagency");
+$con = mysqli_connect("database-1.caav3m5vgnvl.us-east-1.rds.amazonaws.com", "admin", "admin123", "tagency");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
